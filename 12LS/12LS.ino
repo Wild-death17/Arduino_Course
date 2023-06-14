659cd9d09be80c31716e355840b3d2f98e858098
MosheCohen <131193639+Wild-death17@users.noreply.github.com>
2023-06-14 19:33:26 +0300

    Add files via upload

--- 12LS/12LS.ino
+++ 12LS/12LS.ino
@@ -1,36 +1,28 @@
-
-
 // defines pins numbers
-#define trigPin 12
-#define echoPin 11
+const int trigPin = 12;
+const int echoPin = 11;
+#define SoundSpeed 0.034
 // defines variables
-long duration;
 int distance;
+long duration;
+float MaxWaitTimeInCm = 100 * 2 / SoundSpeed;
 void setup() {
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Inputm
-  Serial.begin(9600); // Starts the serial communication
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
 }
-
-float maxLenInMicroSec=50 * 100 / 0.034; // max time we wait for responses. this calculation is for 50 cm
 void loop() {
- 
   sendTrig();
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH,maxLenInMicroSec);
+  duration = pulseIn(echoPin, HIGH, MaxWaitTimeInCm);
   // Calculating the distance
-  distance= duration*0.034/2;
-  // Prints the distance on the Serial Monitor
+  distance = duration * SoundSpeed / 2;
   Serial.print("Distance: ");
   Serial.print(distance);
   Serial.println(" cm");
-  delay(1000);
 }
-void sendTrig(){
-    // Clears the trigPin
+void sendTrig() {
   digitalWrite(trigPin, LOW);
   delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
   digitalWrite(trigPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigPin, LOW);
